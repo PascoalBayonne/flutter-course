e0eedac9edd249b6c0811b21e7a29a22eaadeada
= <=>
2021-11-07 14:00:12 +0000

    init dart and flutter basics

---
+++ flutter_basics/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext buildContext) {
+    return MaterialApp(
+      home: Text('Hello!'),
+    );
+  }
+}
