5d59547f74b4b30a503c568c624f851db1e16695
= <=>
2021-11-11 22:21:22 +0000

    init botton and other widgets

--- flutter_basics/lib/main.dart
+++ flutter_basics/lib/main.dart
@@ -5,10 +5,41 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('The answer has been pressed');
+  }
+
+  var questions = [
+    'What\'s your favorite programming language?',
+    'What\'s your favorite SO'
+  ];
+
   @override
   Widget build(BuildContext buildContext) {
     return MaterialApp(
-      home: Text('Hello!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('iGo'),
+        ),
+        body: Column(
+          children: [
+            Text('The Question'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: answerQuestion,
+            )
+          ],
+        ),
+        // backgroundColor: Color(00),
+      ),
     );
   }
 }
