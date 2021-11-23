ffee24d1074d97766db5e151904075e75d7e2fbc
= <=>
2021-11-23 21:04:29 +0000

    including the stateful design

--- flutter_basics/lib/main.dart
+++ flutter_basics/lib/main.dart
@@ -4,15 +4,28 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  void answerQuestion() {
-    print('The answer has been pressed');
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
   }
+}
 
+class MyAppState extends State<MyApp> {
   var questions = [
-    'What\'s your favorite programming language?',
-    'What\'s your favorite SO'
+    'On which day we had our marriage wedding?',
+    'What\'s your favorite SO',
+    'Mercedes or BMW'
   ];
+
+  var _questionIndex = 0; // the "_" means that its a private field, so it cannot be changed outside (encapsulation)
+  void answerQuestion() {
+    //forces flutter to re(render) the UI. Not the all UI app but the widget that called the setState
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print('the question index is: $_questionIndex ');
+  }
 
   @override
   Widget build(BuildContext buildContext) {
@@ -23,18 +36,21 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('The Question'),
+            Text(questions[_questionIndex]),
             RaisedButton(
               child: Text('Answer 1'),
               onPressed: answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 2'),
+              focusColor: Color(05),
+              color: Color(016),
               onPressed: answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 3'),
-              onPressed: answerQuestion,
+              onPressed: () =>
+                  print('Now you have just printed annonymous function'),
             )
           ],
         ),
