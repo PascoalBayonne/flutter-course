1c6045f2a8465f3b1193286ca7a9b49bae34d9e0
= <=>
2021-11-25 22:40:56 +0000

    adding dynamic answers and learning map data structure

--- flutter_basics/lib/answer.dart
+++ flutter_basics/lib/answer.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
+  final String answerText;
   final VoidCallback selectHandler;
 
-  Answer(this.selectHandler);
+  Answer(this.selectHandler, this.answerText);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       child: RaisedButton(
-        child: Text('Answer Custom'),
+        child: Text(this.answerText),
         color: Colors.blue,
         onPressed: selectHandler,
+        textColor: Colors.white,
       ),
     );
   }
--- flutter_basics/lib/main.dart
+++ flutter_basics/lib/main.dart
@@ -15,9 +15,18 @@ class MyApp extends StatefulWidget {
 
 class MyAppState extends State<MyApp> {
   var questions = [
-    'What is iGo app?',
-    'When it was released?',
-    'Would you recommend'
+    {
+      'Question': 'What is iGo app?',
+      'Answers': ['A date app', 'An app to find pets', 'An app for events']
+    },
+    {
+      'Question': 'When it was released?',
+      'Answers': ['1975', '2077', '2022']
+    },
+    {
+      'Question': 'The dev team are from',
+      'Answers': ['USA', 'Bangladesh', 'Portugal']
+    }
   ];
 
   var _questionIndex =
@@ -34,15 +43,16 @@ class MyAppState extends State<MyApp> {
   Widget build(BuildContext buildContext) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('iGo'),
-        ),
+        appBar: AppBar(title: Text('iGo')),
         body: Column(
           children: [
-            Question(questions[_questionIndex]),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion)
+            Question(
+              questions[_questionIndex]['Question'],
+            ),
+            ...(questions[_questionIndex]['Answers'] as List<String>)
+                .map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()
           ],
         ),
         // backgroundColor: Color(00),
