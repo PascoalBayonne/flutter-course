a562b49e8bded08277c5eee58a77527d4e445148
= <=>
2022-03-14 21:22:00 +0000

    adding Theme the general default color for the app

--- xpenses/lib/main.dart
+++ xpenses/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:iGo/widgets/user_transaction.dart';
+import 'package:iGo/models/transaction.dart';
+import 'package:iGo/widgets/Transation_list.dart';
+import 'package:iGo/widgets/newTransaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,22 +9,65 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "iGo",
+      title: "Personal Expenses",
+      //Theme are very nice to define in one place your app theme. In my case primarySwatch color gonna be the one for the entire app
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  // String titleInput;
-  // String amountInput;
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+        id: 't1',
+        title: 'Diesel 4 Mercedez',
+        amount: 59.99,
+        date: DateTime.now()),
+    Transaction(
+        id: 't2',
+        title: 'NoSolo Italia : Calzone',
+        amount: 6.23,
+        date: DateTime.now()),
+    Transaction(
+        id: 't3', title: 'Jordan Air', amount: 200, date: DateTime.now())
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTX = Transaction(
+        id: DateTime.now().toString(),
+        title: txTitle,
+        amount: txAmount,
+        date: DateTime.now());
+    setState(() {
+      _userTransactions.add(newTX);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return NewTransaction(_addNewTransaction);
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('iGo'),
+        title: const Text('Personal Expenses'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => _startAddNewTransaction(context),
+          )
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -37,9 +82,14 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            UserTransaction()
+            TransactionList(_userTransactions)
           ],
         ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _startAddNewTransaction(context),
       ),
     );
   }
